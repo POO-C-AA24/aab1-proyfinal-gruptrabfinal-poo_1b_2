1c7d69c5dd69719e06412404dff69e155e5251b6
Jean Zapata <jpzapata1@utpl.edu.ec>
2024-07-29 11:12:15 -0500

    Proyecto Final 2

--- Segundo Bimestre/Solucion_Codigo/DeclaracionAnual2/src/View/ImpuestosView.java
+++ Segundo Bimestre/Solucion_Codigo/DeclaracionAnual2/src/View/ImpuestosView.java
@@ -1,4 +1,5 @@
 package View;
+
 import Controller.Impuestos;
 import Model.Usuario;
 import Model.Ingreso;
@@ -6,11 +7,13 @@ import Model.Tipo_Factura;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+
 public class ImpuestosView {
     public void mostrarDeclaracion(Usuario usuario) {
         Impuestos controller = new Impuestos(usuario);
         double impuestos = controller.calcularImpuestos();
         double sueldoAnual = 0;
+        double totalDeducciones = 0; // Variable para el total de deducciones
 
         try (PrintWriter writer = new PrintWriter(new FileWriter("declaracion_impuestos.csv"))) {
             writer.println("Sueldos Mensuales:");
@@ -27,7 +30,11 @@ public class ImpuestosView {
                                ", Monto: " + factura.getMonto() +
                                ", Fecha: " + factura.getFecha() +
                                ", Monto Maximo Deducible: " + factura.getMontoMaximo());
+                totalDeducciones += factura.getMontoMaximo(); // Acumula el total de deducciones
             }
+            
+            writer.println("Total de Deducciones: " + totalDeducciones); // Imprime el total de deducciones
+            
             writer.println("Declaracion de Impuestos");
             writer.println("Nombre: " + usuario.getNombre());
             writer.println("DNI: " + usuario.getDni());
